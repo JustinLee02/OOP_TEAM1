54dea216470795d18081dc2cce1fe0c8975a2031
SOOHYUNLEE08 <lsoo0827@gmail.com>
2024-10-27 17:54:36 +0900

    #9 Feat: 홈 화면 UI 구성

--- KAUPark/app/src/main/java/com/example/kaupark/MainActivity.kt
+++ KAUPark/app/src/main/java/com/example/kaupark/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.viewpager2.widget.ViewPager2
 import com.example.kaupark.databinding.ActivityMainBinding
+import com.example.kaupark.databinding.HomeViewBinding
 import com.example.kaupark.databinding.ParkingAvailableBinding
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
@@ -15,9 +16,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding = ParkingAvailableBinding.inflate(layoutInflater)
+        val binding = HomeViewBinding.inflate(layoutInflater)
         val tr = supportFragmentManager.beginTransaction().run {
-            replace(binding.parkingAvailable.id, ParkingAvailable())
+            replace(binding.homeview.id, HomeView())
             commit()
         }
 
