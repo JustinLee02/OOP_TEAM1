cc47cc07a7db7fb409e8b09a33016fab412db769
JustinLee02 <164829006+JustinLee02@users.noreply.github.com>
2024-10-31 19:33:26 +0900

    #15 Feat: 홈 화면 스크롤 뷰 구현/ 레이아웃 수정 필요

--- KAUPark/app/src/main/java/com/example/kaupark/MainActivity.kt
+++ KAUPark/app/src/main/java/com/example/kaupark/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.viewpager2.widget.ViewPager2
 import com.example.kaupark.databinding.ActivityMainBinding
