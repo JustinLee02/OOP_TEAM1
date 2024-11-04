fba56531cb3ec711181119d60ffc026c54b17f3a
JustinLee02 <164829006+JustinLee02@users.noreply.github.com>
2024-11-04 20:12:53 +0900

    #14 홈 화면 UI 네이버 지도 초기 위치 수정 및 내비게이션 바 추가

---
+++ KAUPark/app/src/main/java/com/example/kaupark/activity/BottomNavigationActivity.kt
@@ -0,0 +1,55 @@
+package com.example.kaupark.activity
+
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.kaupark.R
+import com.example.kaupark.databinding.ActivityBottomNavigationBinding
+import com.example.kaupark.databinding.HomeViewBinding
+import com.example.kaupark.fragment.ChatPopupFragment
+import com.example.kaupark.fragment.HomeView
+import com.example.kaupark.fragment.ParkingAvailable
+
+class BottomNavigationActivity : AppCompatActivity() {
+    private val binding: ActivityBottomNavigationBinding by lazy {
+        ActivityBottomNavigationBinding.inflate(layoutInflater)
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        setupBottomNav()
+
+        if (savedInstanceState == null) {
+            binding.bottomNavigationView.selectedItemId = R.id.fragment_home
+        }
+
+    }
+
+    fun setupBottomNav() {
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.fragment_home -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.main_container, HomeView()).commit()
+                    true
+                }
+                R.id.fragment_park -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.main_container, ParkingAvailable()).commit()
+                    true
+                }
+                R.id.fragment_payment -> {
+                    false
+                }
+                R.id.fragment_chat -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.main_container, ChatPopupFragment()).commit()
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+}
